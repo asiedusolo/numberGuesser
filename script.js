5e5f612d975e93814169c917af807117a9622add
asiedusolo <solomon.asiedu@amalitech.org>
2021-08-06 00:12:01 -0400

    adding function to generate target number and function to compare guesses

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+function generateTarget() {
+  return Math.floor(Math.random() * 10);
+}
 
+function compareGuesses(userGuess, computerGuess, secretNumber) {
+  let computerCloseness = Math.abs(secretNumber - computerGuess);
+  let userCloseness = Math.abs(secretNumber - userGuess);
+  if (computerCloseness === userCloseness) {
+    return true;
+  } else if (computerCloseness > userCloseness) {
+    return true;
+  } else {
+    return false;
+  }
+}
